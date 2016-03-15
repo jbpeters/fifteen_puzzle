221df926de8c18bc5a42eda8158739617a981570
Jeff Peters <jeffp@swva.net>
2016-03-15 14:55:31 -0400

    fixed sliding

--- a.py
+++ a.py
@@ -13,7 +13,6 @@ kolor = [[0 for _ in range(grid)] for _ in range(grid)]
 def init (size,kolor):
     P.display.init()
     P.font.init()
-    #myfont = P.font.SysFont(None,30)
     S = P.display.set_mode((size))
     S.fill(kolor)
     P.display.flip()
@@ -31,11 +30,9 @@ def least(A):
         return 2
 
 def contrast(RGB):
-    #print("RGB = ",RGB, least(RGB))
     Z=[0,0,0]
     C=[127,127,127]
     S=[ C[0]-RGB[0],C[1]-RGB[1],C[2]-RGB[2]]
-    #print("S   = ",S,least(S))
     L=[0,0,0]
     for k in range(3):
         if RGB[k] > 127:
@@ -43,7 +40,6 @@ def contrast(RGB):
         else:
             L[k]=RGB[k]
     ll=least(L)
-    #print("L   = ",L,ll)
     if RGB[ll] <= 127:
         w=255
     else:
@@ -58,12 +54,10 @@ def contrast(RGB):
     return Z
 
 def tile(surf,x,y,width,height,kolor,num,myfont):
-    print(kolor,end="")
     P.draw.rect(surf,kolor,(x,y,width,height))
     if num !="":
         P.draw.rect(surf,[0,0,0],(x,y,width,height),4)
         k2 = contrast(kolor)
-        print(k2)
         surf2 = myfont.render(str(num),1,k2)
         ss = myfont.size(str(num))
         surf.blit(surf2,(x+(width-ss[0])/2,y+(height-ss[1])/2))
@@ -78,10 +72,7 @@ def switch_flip(S, P1, P2, width, height, myfont):
     c2=int(c2)
     r2=int(r2)
     tile(S,c1*width,r1*height,width,height, kolor[c2][r2], num[c2][r2], myfont )
-    #print("tiling ",c2,r2,num[c1][r1])
     tile(S,c2*width,r2*height,width,height, kolor[c1][r1], num[c1][r1], myfont )
-    #print("tiling ",c1,r1,num[c2][r2])
-    #print("switching ", num[c1][r1] , " and ", num[c2][r2] )
     k=kolor[c2][r2]
     kolor[c2][r2] = kolor[c1][r1]
     kolor[c1][r1] = k
@@ -110,13 +101,20 @@ def loop(S,width,height,myfont):
                 atrow = my * grid // box[1]
                 width=box[0]/grid
                 height=box[1]/grid
-                x=atcol*width
-                y=atrow*height
+                #x=atcol*width
+                #y=atrow*height
+                if atcol == target[0] or atrow == target[1]:
+                    if atcol == target[0]-1 or atcol == target[0]+1: 
+                        switch_flip(S,[atcol,atrow],target,width,height,myfont)
+                        target[0]=atcol
+                    if atrow == target[1]-1 or atrow == target[1]+1:
+                        switch_flip(S,[atcol,atrow],target,width,height,myfont)
+                        target[1]=atrow
     P.display.quit()
     return
 
 def main():
-    global num
+    global num,target
     S = init(box,white)
     width=w/grid
     height=h/grid
@@ -124,11 +122,11 @@ def main():
       
     for row in range(grid):
         for col in range(grid):
-            #print(col,row,row*grid+col+1)
             num[col][row] = row*grid+col+1
             if col == grid - 1 and row == grid - 1:
                 kolor[col][row]=white
                 num[col][row]=""
+                target=[col,row]
                 continue
             
             kolor[col][row]=[R.randint(0,255),R.randint(0,255),R.randint(0,255)]
