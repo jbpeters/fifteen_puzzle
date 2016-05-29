a52ced0a58f5e2ac7f5616e9a40074a29976df47
Jeff Peters <jbpeters@users.noreply.github.com>
2016-05-28 21:18:09 -0400

    Delete b.py

--- b.py
+++
@@ -1,9 +0,0 @@
-grid = 4
-num = [ [0 for _ in range(grid)]  for _ in range(grid)]
-def main():
-    for row in range(grid):
-        for col in  range(grid):
-            num[row][col]=( row*grid + col+1)
-    print(num)
-if __name__ =="__main__":
-    main()
